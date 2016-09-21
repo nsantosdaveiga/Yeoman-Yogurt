62bf7406276ae2b55e7f4f7e3ae6ecc37dc48942
nsantosdaveiga <nelson.santosdaveiga@gmail.com>
2016-09-21 12:18:49 +0200

    add integration site sample

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -3,10 +3,10 @@
 
 'use strict';
 
-import $ from 'jquery';
-import Link from '../_modules/link/link';
 
-$(() => {
-  new Link(); // Activate Link modules logic
-  console.log('Welcome to Yeogurt!');
-});
+var $ = require("../../node_modules/foundation-sites/vendor/jquery/dist/jquery.min.js");
+window.jQuery = $;
+
+require('../../node_modules/foundation-sites/dist/foundation.min.js');
+
+$(document).foundation();
