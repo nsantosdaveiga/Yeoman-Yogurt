3ffafb2706032085739359a5931c6937be2b1ec0
nsantosdaveiga <nelson.santosdaveiga@gmail.com>
2016-09-21 17:30:34 +0200

    Nettoyage code inutile :-)

--- src/_modules/vignette/vignette.js
+++ src/_modules/vignette/vignette.js
@@ -4,12 +4,9 @@ import $ from '../../../node_modules/foundation-sites/vendor/jquery/dist/jquery.
 
 export default class Vignette {
   constructor() {
-    this.name = 'vignette';
-    console.log('%s module', this.name);
     this.animate();
   }
   animate() {
-    console.log('me');
     $( ".vignette-container" ).click(function() {
       $( ".vignette" ).animate({
         opacity: 0.25,
--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -5,14 +5,12 @@
 import $ from '../../node_modules/foundation-sites/vendor/jquery/dist/jquery.min.js';
 import Vignette from '../_modules/vignette/vignette';
 
-//var $ = require("../../node_modules/foundation-sites/vendor/jquery/dist/jquery.min.js");
 window.jQuery = $;
 
 require('../../node_modules/foundation-sites/dist/foundation.min.js');
 
 
 $(() => {
-  console.log('Welcome to Yeogurt!');
   new Vignette();
 });
 
